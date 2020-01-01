79566589ac0629ec8005508b969219575a164844
natsume0718 <moto718program@gmail.com>
2020-01-01 22:43:49 +0900

    不要コメント削除 ルーティングをprefixで省略

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
- */
-
 Route::get('/', function () {
     return view('top', ['user' => Auth::user()]);
 })->name('top');
-Route::prefix('auth/twitter')->group(function () {
+
+Route::group(['prefix' => 'auth/twitter'], function () {
     // ログインURL
     Route::get('/', 'Auth\LoginController@redirectToProvider')->name('login');
     // コールバックURL
@@ -22,11 +12,11 @@ Route::prefix('auth/twitter')->group(function () {
     // ログアウトURL
     Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 });
-Route::group(['middleware' => ['auth', 'user.name'], 'prefix' => 'activity'], function () {
-    Route::get('{user_name}/', 'ActivityController@index')->name('activity.index');
-    Route::post('{user_name}/', 'ActivityController@store')->name('activity.store');
-    Route::get('{user_name}/{activity}', 'ActivityController@show')->name('activity.show');
-    Route::patch('{user_name}/{activity}', 'ActivityController@update')->name('activity.tweet');
-    Route::delete('{user_name}/{activity}', 'ActivityController@destroy')->name('activity.delete');
-    Route::delete('{user_name}/{activity}/{id}', 'ActivityController@deleteTweet')->name('tweet.delete');
+Route::group(['prefix' => '{user_name}', 'middleware' => ['auth', 'user.name']], function () {
+    Route::get('/', 'ActivityController@index')->name('activity.index');
+    Route::post('/', 'ActivityController@store')->name('activity.store');
+    Route::get('/{activity}', 'ActivityController@show')->name('activity.show');
+    Route::patch('/{activity}', 'ActivityController@update')->name('activity.tweet');
+    Route::delete('/{activity}', 'ActivityController@destroy')->name('activity.delete');
+    Route::delete('/{activity}/{id}', 'ActivityController@deleteTweet')->name('tweet.delete');
 });
