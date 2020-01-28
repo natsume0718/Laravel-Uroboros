245de2c4a40f678735f925e2847fb8dd80064062
natsume0718 <moto718program@gmail.com>
2020-01-29 00:28:56 +0900

    ログ追加

--- app/Http/Middleware/UserNameMiddleware.php
+++ app/Http/Middleware/UserNameMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class UserNameMiddleware
 {
@@ -19,6 +20,7 @@ class UserNameMiddleware
         $user = $request->user();
         //ログインしていない、ユーザー名が一致していない場合
         if (!$user || $request->user_name !== $user->nickname) {
+            Log::notice($user->nickname . ':がアクセス:' . $request->url());
             return redirect()->back();
         }
         return $next($request);
