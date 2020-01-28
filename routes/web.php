6bc17595a8d3369c3c762d07da022a2b1fced2a1
natsume0718 <moto718program@gmail.com>
2020-01-29 00:32:14 +0900

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,6 @@ Route::group(['prefix' => 'auth/twitter'], function () {
 });
 Route::group(['prefix' => '{user_name}', 'middleware' => ['auth', 'user.name']], function () {
     Route::resource('activity', 'ActivityController')->only(['index', 'store', 'show', 'update', 'destroy']);
+    Route::resource('activity/{activity}/post', 'PostController')->only(['store', 'destroy']);
     Route::delete('tweet/{id}', 'ActivityController@deleteTweet')->name('tweet.destroy');
 });
