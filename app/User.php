c8eb68707f9ac98b73a8aed83dfa1bf66987a05f
natsume0718 <moto718program@gmail.com>
2020-01-29 00:27:10 +0900

    rememberToken無効化をなしに

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
     use Notifiable;
     use SoftDeletes;
 
-    protected $rememberTokenName = false;
+    // protected $rememberTokenName = false;
 
     /**
      * The attributes that are mass assignable.
