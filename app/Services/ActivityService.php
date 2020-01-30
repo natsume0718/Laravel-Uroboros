3e8d496f770f92500036ebcb31917a9d3f1c993b
natsume0718 <moto718program@gmail.com>
2020-01-31 01:18:16 +0900

    不要な処理の削除

--- app/Services/ActivityService.php
+++ app/Services/ActivityService.php
@@ -27,7 +27,6 @@ class ActivityService
     public function createActivity(array $data)
     {
         $user = Auth::user();
-        Log::info(Auth::id() . ":新規投稿:" . json_encode($data));
         $activity = $user->activities()->create($data);
         return (bool) $activity;
     }
@@ -43,48 +42,9 @@ class ActivityService
         return Activity::with(['posts'])->columns()->where('user_id', $user->id)->where('name', $name)->first();
     }
 
-    /**
-     * 継続日数増加
-     * @param int $activity_id
-     * @param int $day
-     * @return bool
-     */
-    public function incrementContinuationDays(int $activity_id, int $day = 1)
-    {
-        Log::info(Auth::id() . ':継続日数増加:' . $activity_id);
-        $result = Activity::whereActivity($activity_id)->whereincrement('continuation_days', $day);
-        return (bool) $result;
-    }
-
-    /**
-     * 継続日数リセット
-     * @param int $activity_id
-     * @return bool
-     */
-    public function resetContinuationDays(int $activity_id)
-    {
-        Log::info(Auth::id() . ':継続日数リセット:' . $activity_id);
-        $result = Activity::whereActivity($activity_id)->update(['continuation_days' => 0]);
-        return (bool) $result;
-    }
-
-    /**
-     * 活動日数増加
-     * @param int $activity_id
-     * @param int $day
-     * @return bool
-     */
-    public function incrementActivityDays(int $activity_id, int $day = 1)
-    {
-        Log::info(Auth::id() . ':活動日数増');
-        $result = Activity::whereActivity($activity_id)->increment('activity_days', $day);
-        return (bool) $result;
-    }
-
     public function deleteById(int $id)
     {
         $user = Auth::user();
-        Log::info(Auth::id() . ':活動削除');
         $activity = $user->activities()->findOrFail($id);
         return $activity->delete();
     }
