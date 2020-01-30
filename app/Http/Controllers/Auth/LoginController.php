6772b22ab1c1f0d25ed6efd2f7d8a4754290bbe3
natsume0718 <moto718program@gmail.com>
2020-01-31 02:27:43 +0900

    リファクタ

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,8 +39,11 @@ class LoginController extends Controller
         // twitterからアカウント情報取得
         try {
             $account_info = Socialite::driver('twitter')->user();
-            DB::beginTransaction();
-            // ユーザーの登録・更新
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return redirect()->route('top')->with('error', 'Twitterアカウント取得に失敗しました');
+        }
+        $auth_user = DB::transaction(function () use ($account_info) {
             $user = User::updateOrCreate(['id' => $account_info->id], [
                 'name' => $account_info->name,
                 'nickname' => $account_info->nickname,
@@ -48,20 +51,11 @@ class LoginController extends Controller
                 'token' => $account_info->token,
                 'token_secret' => $account_info->tokenSecret
             ]);
-
-            if (!isset($user)) {
-                throw new ModelNotFoundException('ユーザー情報の生成または更新の失敗');
-            }
-            DB::commit();
-            // ログイン
             Auth::login($user, true);
-            Log::info($user->nickname . ' :login');
-            return redirect()->route('activity.index', $user->nickname)->with('success', 'ログインしました');
-        } catch (\Exception $e) {
-            DB::rollback();
-            Log::error($e->getMessage());
-            return redirect()->route('top')->with('error', 'Twitterアカウント取得に失敗しました');
-        }
+            return $user;
+        });
+
+        return redirect()->route('activity.index', $auth_user->nickname)->with('success', 'ログインしました');
     }
 
     /**
