7ea9794d248fc0d26ed51f41d3de580b7230253c
natsume0718 <moto718program@gmail.com>
2020-02-02 02:27:13 +0900

    remove import

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\{Auth, DB, Log};
 use App\User;
 use Illuminate\Http\Request;
